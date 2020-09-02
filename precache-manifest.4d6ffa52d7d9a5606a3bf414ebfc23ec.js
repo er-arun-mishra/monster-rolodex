a9dbf1dc5bf541807ccb5e0e346f8d932c3d951e
unknown <Client@EBOMREN302NTD.ad.dspim.com>
2020-09-02 18:16:53 +0530

    Updates

---
+++ precache-manifest.4d6ffa52d7d9a5606a3bf414ebfc23ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec65f4e0dc108dbdcf4204d42d81e24f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3712d7ecb58de586e0a7",
+    "url": "/monster-rolodex/static/css/main.2a95efc3.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monster-rolodex/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3712d7ecb58de586e0a7",
+    "url": "/monster-rolodex/static/js/main.2b0e3f16.chunk.js"
+  },
+  {
+    "revision": "fa483a6908201fa18e92",
+    "url": "/monster-rolodex/static/js/runtime-main.3adb4eac.js"
+  }
+]);
